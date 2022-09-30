67523ef9fa3e22d030c7412c868505a22ec4f42a
Denishilo <denisanishenko@gmail.com>
2022-09-30 23:37:12 +0300

    dop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Button} from "./UniversalComponents/Button";
+
+
+type DataPropsType = {
+    body: string
+    id: number
+    title: string
+    userId: number
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState<DataPropsType[]>([])
+
+    const getMeData = () => {
+        setData([])
+    }
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setData(json))
+    },[])
+
+    return (
+        <>
+            <Button name={'Clean Data'} callback={getMeData}/>
+            <ul>
+                {data.map(el => {
+                    return (
+                        <li key={el.id}>{el.title}</li>
+                    )
+                })}
+            </ul>
+        </>
+    );
 }
 
 export default App;
---
+++ src/UniversalComponents/Button.tsx
@@ -0,0 +1,17 @@
+
+type ButtonPropsType = {
+    name:string
+    callback:()=>void
+}
+
+export const Button=(props:ButtonPropsType)=>{
+    const {name,callback} = props
+
+    const onClickHandler = ()  =>{
+        callback()
+    }
+
+    return (
+        <button onClick={onClickHandler}>{name}</button>
+    )
+}
